9a4e6e61a53318139549b6182e138677dbf425ea
Adm <vanderman789@gmail.com>
2026-02-09 21:11:02 +0300

    add readme.md

--- src/test/java/web/pages/ProductPageObject.java
+++ src/test/java/web/pages/ProductPageObject.java
@@ -7,7 +7,6 @@ import io.qameta.allure.Step;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverConditions.urlContaining;
 
 public class ProductPageObject {
 
