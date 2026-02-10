1a867f9fc20229250ed3f19fc0556d211e8f915b
Adm <vanderman789@gmail.com>
2026-02-10 21:50:13 +0300

    optinize

--- src/test/java/web/pages/SearchResultsPageObject.java
+++ src/test/java/web/pages/SearchResultsPageObject.java
@@ -1,14 +1,16 @@
 package web.pages;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchResultsPageObject {
-
     private final ElementsCollection productLinks = $$x(
             "//main//a[@href and " +
                     "not(starts-with(@href,'#')) and " +
@@ -19,21 +21,30 @@ public class SearchResultsPageObject {
                     "not(contains(@href,'/compare')) and " +
                     "not(contains(@href,'/page/'))" +
                     "]"
-    ).filter(visible);
+    );
+
+    private SelenideElement firstVisibleProductLink() {
+        productLinks.shouldHave(sizeGreaterThan(0), Duration.ofSeconds(30));
+        return productLinks.filter(visible).first().shouldBe(visible, Duration.ofSeconds(30));
+    }
 
     @Step("Проверить, что результаты поиска отображаются")
     public SearchResultsPageObject resultsExist() {
-        productLinks.shouldHave(sizeGreaterThan(0));
+        productLinks.filter(visible).shouldHave(sizeGreaterThan(0), Duration.ofSeconds(30));
         return this;
     }
 
     @Step("Открывается первый товар из списка")
     public ProductPageObject openFirstProduct() {
-        productLinks.shouldHave(sizeGreaterThan(0));
-        productLinks.first().scrollIntoView("{block:'center'}").click();
+        SelenideElement first = firstVisibleProductLink();
+        executeJavaScript("arguments[0].scrollIntoView({block:'center', inline:'nearest'});", first);
+        sleep(200);
+        executeJavaScript("arguments[0].click();", first);
+
         return new ProductPageObject();
     }
 }
+
 
 
 
